fa01585d4ffefe4bad1dd2675118b572d66e7463
David Poslt <david.poslt@gmail.com>
2020-01-21 21:21:16 +0100

    change format

--- size.py
+++ size.py
@@ -2,7 +2,7 @@
 def convert_bytes(size):
    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
        if size < 1024.0:
-           return "%3.1f %s" % (size, x)
+           return "%1.2f %s" % (size, x)
        size /= 1024.0
 
    return size
